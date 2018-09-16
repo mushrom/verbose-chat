2d5063de80451ae61ce8249eed28e4c27506a2fc
My Name <foo@lisp.us.to>
2018-09-16 07:42:42 +0000

    More stuff

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,10 +3,19 @@ import React from "react";
 class FooBar extends React.PureComponent {
     render() {
         return (
-            <div style={{textAlign: "center"}}>
+            <div> {this.props.text} </div>
+        );
+    }
+}
+
+class Column extends React.PureComponent {
+    render() {
+        return (
+            <div class="col-md-4">
+                <h2>{this.props.title}</h2>
                 {this.props.text}
             </div>
-        )
+        );
     }
 }
 
@@ -14,9 +23,72 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <FooBar text="Hello, world!" />
-                <br />
-                <FooBar text="testing this..." />
+                <div class="jumbotron">
+                    <h1 class="display-3">
+                        <FooBar text="Hello, world!" />
+                    </h1>
+
+                    <p class="lead">
+                        <FooBar text="testing this..." />
+                    </p>
+
+                    <a class="btn btn-primary btn-lg"
+                       href="#" role="button">
+                        Learn more
+                    </a>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h2>Testing this</h2>
+                            Testing this thing here,
+                            this is a test of bootstrap
+                            in order to see how it
+                            works.
+                        </div>
+
+                        <div class="col-md-4">
+                            <h2>Seems bretty gud</h2>
+                            So far it seems pretty
+                            straight-forward and easy to
+                            use with react
+                        </div>
+
+                        <div class="col-md-4">
+                            <h2>thumbs skyward</h2>
+                            And hopefully it's smooth
+                            sailing from here to working
+                            on verbose.chat
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h2>Testing this</h2>
+                            Testing this thing here,
+                            this is a test of bootstrap
+                            in order to see how it
+                            works.
+                        </div>
+
+                        <div class="col-md-4">
+                            <h2>Seems bretty gud</h2>
+                            So far it seems pretty
+                            straight-forward and easy to
+                            use with react
+                        </div>
+
+                        <div class="col-md-4">
+                            <h2>thumbs skyward</h2>
+                            And hopefully it's smooth
+                            sailing from here to working
+                            on verbose.chat
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
