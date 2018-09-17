ac39c387e12dfb1fc3c4a6d539fcc4dfe1337e7d
My Name <foo@lisp.us.to>
2018-09-17 18:36:06 +0000

    Doing proper debug/production mode settings, setting mode to production by default

--- backend/index_page/urls.py
+++ backend/index_page/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 # TODO: remove this in production!
 from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('layout_test', views.layout_test, name='layout_test'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
