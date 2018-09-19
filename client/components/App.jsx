5bc8390ab135662b447ae4f28e24adab8dd520b0
My Name <foo@lisp.us.to>
2018-09-19 04:45:44 +0000

    More layout stuff

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
-class FooBar extends React.PureComponent {
+class Message extends React.PureComponent {
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <div onClick={this.props.onClick}> {this.props.text} </div>
+            <li class="list-group-item" onClick={this.props.onClick}>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-2"><b>{this.props.author}</b></div>
+                        <div class="col">
+                            <small>Posted on {this.props.date}</small>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-2"><small>&gt;&gt;</small></div>
+                        <div class="col">
+                            {this.props.text}
+                        </div>
+                    </div>
+                </div>
+            </li>
         );
     }
 }
@@ -15,12 +31,12 @@ class FooBar extends React.PureComponent {
 class Sidebar extends React.PureComponent {
     render() {
         return (
-            <div class="col-12 col-md-2">
-                <ul>
-                    <li>#channel</li>
-                    <li>#programming</li>
-                    <li>#longer-channel-name-ok</li>
-                    <li>#thumbs-skyward</li>
+            <div class="col-12 col-md-2 p-0 m-0">
+                <ul class="list-group">
+                    <li class="list-group-item">#channel</li>
+                    <li class="list-group-item">#programming</li>
+                    <li class="list-group-item">#longer-channel-name-ok</li>
+                    <li class="list-group-item">#thumbs-skyward</li>
                 </ul>
             </div>
         );
@@ -43,40 +59,23 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            counter: 1,
+            counter: 15,
         }
     }
 
     handle_click() {
         console.log("got here!");
-        //this.state.counter++;
         this.setState(({counter: this.state.counter + 1}));
     }
 
-    render_foobar(text) {
-        return <FooBar text={text} onClick={() => this.handle_click()} />
-    }
-
-    render_leads() {
+    render_messages() {
         var ret = [];
 
         for (var i = this.state.counter; i != 0; i--) {
             ret = ret.concat([
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-2"><b>John Doe</b></div>
-                        <div class="col">
-                            <small>Posted on Tue Sep 18 08:15:34 UTC 2018</small>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-2"><small>&gt;&gt;</small></div>
-                        <div class="col">
-                            {this.render_foobar("Testing this: " + this.state.counter)}
-                        </div>
-                    </div>
-                </div>
+                <Message author="John Doe" date="Tuesday"
+                         onClick={() => this.handle_click()}
+                         text={"Testing this, testing more stuff, asdf: " + i} />
             ]);
         }
 
@@ -88,9 +87,11 @@ export default class App extends React.Component {
             <div class="row">
                 <Sidebar />
 
-                <div class="col-12 col-md-10">
+                <div class="col-12 col-md-10 p-0 m-0">
                     <div class="container-fluid scroll-overflow verbose-content-box">
-                        {this.render_leads()}
+                        <ul class="list-group">
+                            {this.render_messages()}
+                        </ul>
                     </div>
 
                     <InputBox />
