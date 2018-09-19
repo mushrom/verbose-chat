6c88c5eb8a2201015e050d7b248f426648285942
My Name <foo@lisp.us.to>
2018-09-19 01:53:12 -0700

    Starting to implement login pages

--- backend/user_stuff/urls.py
+++ backend/user_stuff/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="user_stuff")
+    path('', views.index, name="user_stuff"),
+    path('login', views.login, name="login")
 ]
--- backend/user_stuff/views.py
+++ backend/user_stuff/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("User profile/signup page here")
+
+def login(request):
+    return render(request, "user_stuff/login.html")
