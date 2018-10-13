4e363a6a5351a0ec4867b83052f6cf584bf2217f
My Name <foo@lisp.us.to>
2018-10-13 08:03:10 +0000

    Better time formatting in message posts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,31 +2,34 @@ import React from "react";
 import Fetch from "react-fetch"
 import VerbAPI from "../verbapi.js"
 
+function newer_than_a_day(adate) {
+    const one_day = 1000 * 60 * 60 * 24;
+    return Date.now() - adate.getTime() < one_day;
+}
+
 class Message extends React.PureComponent {
     constructor(props) {
         super(props);
     }
 
     render() {
+        const date = new Date(this.props.message.date_posted);
+        const fmt = newer_than_a_day(date)
+                        ? date.toLocaleTimeString()
+                        : date.toLocaleDateString();
+
         return (
             <li class="list-group-item">
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col">
-                            <small></small>
                             <b>{this.props.message.username} </b>
                             <small>@{this.props.message.username}</small>
+                            <small class="float-right">{fmt}</small>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col">
-                            <small>Posted on {this.props.message.date_posted}</small>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-1"><small>&gt;&gt;</small></div>
                         <div class="col">
                             {this.props.message.content}
                         </div>
